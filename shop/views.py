d38bbcbe38ad10f4f356e96c7d0c43617e32cd9c
Kamogelo25 <kamogelomalope25@gmail.com>
2022-02-10 11:38:01 +0200

    added contact page

--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def HomePageView(request):
     return render(request, 'home.html')
+
+def ContactPageView(request):
+    return render(request, 'pages/contact.html')    
