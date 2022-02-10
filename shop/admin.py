2c0d25dd08fc59aa2885ba8f7487084c44d70e5c
Bafo <sshabangu061@gmail.com>
2022-02-10 12:02:24 +0200

    Models and Admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from  .models import Category, Product
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_field = {'slug': ('name')}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'image', 'category', 'availability', 'slug')
+    list_filter = ['availability']
+    list_editable = ('price', 'availability')
+    search_field = ('name', 'category', 'availability')
+    prepopulated_field = {'slug': ('name')}
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,36 @@
+from distutils.command.upload import upload
+from traceback import print_exception
 from django.db import models
 
+
+
+
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    description = models.TextField(blank=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to = 'images/', default='', null = True, blank = True)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    availability = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
