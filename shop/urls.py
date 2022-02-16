4954035e9aa699f44ccf930b26f455fd24dd31f4
Aimua Jeff <aimua_jeff@yahoo.com>
2022-02-16 10:34:40 +0200

    this is resolved

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,22 @@
+from unicodedata import name
 from .views import *
 from django.urls import path
 
 app_name = 'shop'
 urlpatterns=[
 path('', HomePageView, name='home'),
+<<<<<<< HEAD
 
+=======
+path('contact', ContactPageView, name = 'contact'),
+<<<<<<< HEAD
+<<<<<<< HEAD
+path('categories',CatogoriesPageView ,name= 'categories')
+=======
+path('cart', CartPageView, name = 'cart'),
+>>>>>>> upstream/main
+=======
+path('cart', CartPageView, name = 'cart'),
+>>>>>>> upstream/main
+>>>>>>> fa13979283c4d98dd86a94c7d09cfd2e68243bea
 ]
--- shop/views.py
+++ shop/views.py
@@ -5,8 +5,29 @@ def HomePageView(request):
     return render(request, 'home.html')
 
 def ContactPageView(request):
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+    return render(request, 'pages/contact.html')
+
+def CatogoriesPageView(request):
+    return render(request,'pages/categories.html')    
+=======
+=======
+>>>>>>> upstream/main
+>>>>>>> fa13979283c4d98dd86a94c7d09cfd2e68243bea
     return render(request, 'pages/contact.html') 
 
 
 def CartPageView(request):
+<<<<<<< HEAD
     return render(request, 'pages/cart.html')      
+=======
+<<<<<<< HEAD
+    return render(request, 'pages/cart.html')      
+>>>>>>> upstream/main
+=======
+    return render(request, 'pages/cart.html')      
+>>>>>>> upstream/main
+>>>>>>> fa13979283c4d98dd86a94c7d09cfd2e68243bea
